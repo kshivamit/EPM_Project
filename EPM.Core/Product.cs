26ea74e39c722f8c521e32aecc74f114916ff0fb
Shivam Kasaudhan <77121077+kshivamit@users.noreply.github.com>
2021-09-08 23:17:10 +0530

    Add files via upload

---
+++ EPM.Core/Product.cs
@@ -0,0 +1,16 @@
+﻿using EPM.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPM.Core.ProductManagement
+{
+    public class Product : BaseModel<int>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ProductImage { get; set; }
+        public string ProductVideo { get; set; }
+    }
+}
